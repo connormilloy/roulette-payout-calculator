1069189e5dcd65bc2f6f7dd58156ffb836f42c12
connormilloy <74675162+connormilloy@users.noreply.github.com>
2021-03-02 13:36:58 +0000

    Add project files.

---
+++ payoutCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Roulette_Payout_Calculator
+{
+    public partial class payoutCalculator : Form
+    {
+        public payoutCalculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
